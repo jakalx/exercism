84038251376f09b0cb54f701dae3a637a066f4c2
Alexander Kobjolke <me@failco.de>
2023-02-02 22:50:16 +0100

    Setup exercism for Rust

---
+++ devenv.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git pkgs.exercism ];
+
+  enterShell = "";
+
+  starship.enable = true;
+
+  # https://devenv.sh/languages/
+  languages.nix.enable = true;
+  languages.rust.enable = true;
+  languages.elm.enable = true;
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = "echo hello from $GREET";
+
+  # https://devenv.sh/pre-commit-hooks/
+  pre-commit.hooks.shellcheck.enable = true;
+  pre-commit.hooks.rustfmt.enable = true;
+  pre-commit.hooks.clippy.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+}
