5d5e6b2cd201ffd926968dc3c94caf1edb363601
Alexander Kobjolke <me@failco.de>
2024-05-30 13:59:52 +0200

    rust: Solve gigaseconds

--- devenv.nix
+++ devenv.nix
@@ -5,16 +5,17 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git pkgs.exercism ];
+  packages = [ pkgs.git pkgs.exercism pkgs.ghc pkgs.ghcid pkgs.haskell-language-server ];
 
   enterShell = "";
 
-  starship.enable = true;
+#  starship.enable = true;
 
   # https://devenv.sh/languages/
   languages.nix.enable = true;
   languages.rust.enable = true;
   languages.elm.enable = true;
+  languages.haskell.enable = true;
 
   # https://devenv.sh/scripts/
   # scripts.hello.exec = "echo hello from $GREET";
