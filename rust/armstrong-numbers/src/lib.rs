992f39c83375884f12befbbe18bb3fb1073fa860
Alexander Kobjolke <me@failco.de>
2024-06-01 19:37:42 +0200

    rust: Solve armstrong-numbers via to_string

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,22 +1,12 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    let v: Vec<_> = digits(num as u64).collect();
-    num as u64 == v.iter().map(|x| x.pow(v.len() as u32)).sum()
+    let num: u64 = num.into();
+    let v: Vec<_> = digits(num);
+    num == v.iter().map(|x| x.pow(v.len() as u32)).sum()
 }
 
-fn digits(mut num: u64) -> impl Iterator<Item = u64> {
-    let mut divisor = 1;
-    while num >= divisor * 10 {
-        divisor *= 10;
-    }
-
-    std::iter::from_fn(move || {
-        if divisor == 0 {
-            None
-        } else {
-            let v = num / divisor;
-            num %= divisor;
-            divisor /= 10;
-            Some(v)
-        }
-    })
+fn digits(num: u64) -> Vec<u64> {
+    num.to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap().into())
+        .collect()
 }
